9007b1e1a35e9011ace35a6501ce75607ea4b60b
marushkevych <marushkevych@gmail.com>
2016-02-19 00:32:54 -0500

    greeting directive

--- src/app/features/home/index.js
+++ src/app/features/home/index.js
@@ -6,9 +6,10 @@ import routing from './home.routes';
 import HomeController from './home.controller';
 
 import randomNames from '../../services/randomNames.service';
+import greeting from '../../directives/greeting.directive';
 
 
-export default angular.module('app.home', [uirouter, randomNames])
+export default angular.module('app.home', [uirouter, randomNames, greeting])
   .config(routing)
   .controller('HomeController', HomeController)
   .name;
